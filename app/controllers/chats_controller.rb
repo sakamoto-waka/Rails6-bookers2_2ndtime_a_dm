e52da8d7a6d3756c763e11209819cac09e4eee9b
sakamoto-waka <wakanmhl@gmail.com>
2022-04-08 02:59:58 +0000

    [update] validation to :message

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,6 +1,8 @@
 class ChatsController < ApplicationController
   def show
     @user = User.find(params[:id])
-    rooms = current_user.user_rooms
+    # pluckは配列で取得
+    rooms = current_user.user_rooms.pluck(:room_id)
+    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
   end
 end

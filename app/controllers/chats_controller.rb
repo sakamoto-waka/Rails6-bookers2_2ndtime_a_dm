a9c5f059ab64e6226a762c930cfc96a41eafbc53
sakamoto-waka <wakanmhl@gmail.com>
2022-04-08 03:12:05 +0000

    [update] chats#show

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -3,6 +3,20 @@ class ChatsController < ApplicationController
     @user = User.find(params[:id])
     # pluckは配列で取得
     rooms = current_user.user_rooms.pluck(:room_id)
-    user_rooms = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    # roomsにはたくさん部屋があるけどuser_idに合致するroomはひとつなのでfind_byを使ってる
+    user_room = UserRoom.find_by(user_id: @user.id, room_id: rooms)
+    
+    if user_room.nil?
+      @room = Room.new
+      @room.save
+      # 自分と相手の＊2人分＊を中間テーブルに保存しておく
+      UserRoom.create(user_id: @user.id, room_id: @room.id)
+      UserRoom.create(user_id: current_user.id, room_id: @room_id)
+    else
+      @room = user_room.room
+    end
+    # チャット履歴の取得+新規送信するためのインスタンス作成
+    @chats = @room.chats
+    @chat = Chat.new(room_id: @room.id)
   end
 end
